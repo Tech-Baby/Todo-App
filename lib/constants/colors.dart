a825fc89c33126e633c0fddc7a05cd2916a2c1c8
olamide <falowoolamide893@gmail.com>
2023-07-30 08:23:46 -0700

    Simple Todo App

---
+++ lib/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+final customShadowColor = Color(0xFF888888);
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+
+const Color tdBGColor = Color(0xFFEEEFF5);
+
+const Color Gray = Color(0xFF607D8B); // A mid-tone gray color
+const Color darkGray = Color(0xFF333333); // A dark gray color
+const Color LightGray = Color(0xFFB0BEC5); // A light gray color
+const Color Blue = Color(0xFF2196F3); // A vibrant blue color
+const Color Green = Color(0xFF4CAF50); // A fresh green color
+const Color Purple = Color(0xFF9C27B0); // A deep purple color
+const Color Orange = Color(0xFFFF9800); // A warm orange color
+const Color Red = Color(0xFFF44336); // A bold red color
+const Color Teal = Color(0xFF009688); // A refreshing teal color
+const Color Indigo = Color(0xFF3F51B5); // A rich indigo color
+const Color Amber = Color(0xFFFFC107); // A bright amber color
+const Color Pink = Color(0xFFE91E63); // A lovely pink color
+const Color DeepOrange = Color(0xFFFF5722); // A deep orange color
---
+++ lib/screens/home.dart
@@ -0,0 +1,206 @@
+import "dart:ffi";
+
+import "package:flutter/material.dart";
+import "package:flutter_todoapp/constants/colors.dart";
+import "package:flutter_todoapp/widgets/todo_item.dart";
+import "../model/todo.dart";
+import '../widgets/todo_item.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final todosList = Todo.todoList();
+  List<Todo> _foundTodo = [];
+  final _todoController = TextEditingController();
+  @override
+  void initState() {
+    _foundTodo = todosList;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'TODO LIST',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      for (Todo todo in _foundTodo.reversed)
+                        TodoItem(
+                          todo: todo,
+                          onTodoChanged: _handleTodoChange,
+                          onDeleteItem: _deleteTodoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                          hintText: 'Add a new todo item', border: InputBorder.none),
+                    ),
+                    // Add your content here, for example, a button
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    onPressed: () {
+                      _addTodoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      elevation: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteTodoItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _addTodoItem(String todo) {
+    setState(() {
+      todosList.add(Todo(id: DateTime.now().millisecondsSinceEpoch.toString(), todoText: todo));
+    });
+    _todoController.clear();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Todo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.todoText!.toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _foundTodo = results;
+    });
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Expanded(
+            // or Flexible(fit: FlexFit.tight) instead of Expanded
+            child: Center(
+              // Center the text within the available space
+              child: Text(
+                'TODO APP',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35, color: tdBlack),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
